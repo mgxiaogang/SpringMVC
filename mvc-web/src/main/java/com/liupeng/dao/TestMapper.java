35d3fd68ab4e795d6d0187e531437e7824ef2a03
刘鹏 <294333475@qq.com>
2018-02-08 01:14:18 +0800

    init project

--- mvc-web/src/main/java/com/liupeng/dao/TestMapper.java
+++ mvc-web/src/main/java/com/liupeng/dao/TestMapper.java
@@ -1,4 +1,7 @@
 package com.liupeng.dao;
 
+import com.liupeng.jdbc.Person;
+
 public interface TestMapper {
+    Person queryPerson();
 }
--- mvc-web/src/main/java/com/liupeng/jdbc/PersonDaoImpl.java
+++ mvc-web/src/main/java/com/liupeng/jdbc/PersonDaoImpl.java
@@ -1,13 +1,20 @@
 package com.liupeng.jdbc;
 
+import com.liupeng.dao.TestMapper;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class PersonDaoImpl implements IPersonDao {
 
+    @Resource
+    private TestMapper testMapper;
+
     @Override
     public void add() {
         System.out.println("add");
+
     }
 
     @Override
@@ -17,6 +24,7 @@ public class PersonDaoImpl implements IPersonDao {
 
     @Override
     public Person query() {
-        return null;
+        //return testMapper.queryPnoerson();
+        return new Person();
     }
 }
