2c5f9700d1e323ac6eb01bc3e8da85fd4c8e9d35
枫道 <fengdao.lp@alibaba-inc.com>
2018-05-15 16:30:13 +0800

    修改BeaninitializationPerformanceMonitor

--- mvc-service/src/main/java/com/liupeng/spring/BeaninitializationPerformanceMonitor.java
+++ mvc-service/src/main/java/com/liupeng/spring/BeaninitializationPerformanceMonitor.java
@@ -1,38 +1,82 @@
 package com.liupeng.spring;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
+
+import static com.google.common.collect.Lists.newArrayListWithCapacity;
+import static com.google.common.collect.Maps.newHashMapWithExpectedSize;
+import static java.util.Collections.reverseOrder;
 
 /**
  * @author fengdao.lp
  * @date 2018/5/15
  */
 @Service
-public class BeaninitializationPerformanceMonitor implements BeanPostProcessor {
+public class BeaninitializationPerformanceMonitor implements BeanPostProcessor, ApplicationListener {
 
-    private final Logger logger = LoggerFactory.getLogger(BeaninitializationPerformanceMonitor.class);
+    public static Log log = LogFactory.getLog(BeaninitializationPerformanceMonitor.class);
 
-    private Map<String, Long> staticsMaps = new ConcurrentHashMap<String, Long>();
+    private Map<String, Long> startTime = newHashMapWithExpectedSize(1024);
+    private List<Initialization> costTime = newArrayListWithCapacity(1024);
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        staticsMaps.put(beanName, System.currentTimeMillis());
-        logger.error("id={},class={}", beanName, bean.getClass().getName());
+        startTime.put(beanName, System.currentTimeMillis());
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (staticsMaps.get(beanName) != null) {
-            Long t = System.currentTimeMillis() - staticsMaps.get(beanName);
-            logger.error("bean初始化,id={},class={},t={}ms", beanName, bean.getClass().getName(), t);
+        Long start = startTime.get(beanName);
+        if (start != null) {
+            costTime.add(Initialization.of(beanName, (System.currentTimeMillis() - start)));
+            startTime.remove(beanName);
         }
         return bean;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ContextRefreshedEvent) {
+            Collections.sort(costTime, reverseOrder());
+            for (Initialization initialization : costTime) {
+                log.info(initialization);
+            }
+            startTime.clear();
+            costTime.clear();
+        }
+    }
+
+    private static class Initialization implements Comparable<Initialization> {
+        private String beanName;
+        private long costTime;
+
+        public static Initialization of(String beanName, long costTime) {
+            Initialization initialization = new Initialization();
+            initialization.costTime = costTime;
+            initialization.beanName = beanName;
+            return initialization;
+        }
+
+        @Override
+        public String toString() {
+            return "bean:" + beanName + ",cost " + costTime + " ms.";
+        }
+
+        @Override
+        public int compareTo(Initialization o) {
+            long res = costTime - o.costTime;
+            return res == 0 ? 0 : (res > 0 ? 1 : -1);
+        }
     }
 }
