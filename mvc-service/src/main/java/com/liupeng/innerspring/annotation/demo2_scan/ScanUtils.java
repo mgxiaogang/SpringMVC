8253ca3e5e1eb27efe6869ec3439a0f8fcab3df9
刘鹏 <294333475@qq.com>
2018-12-26 17:48:28 +0800

    spring

---
+++ mvc-service/src/main/java/com/liupeng/innerspring/annotation/demo2_scan/ScanUtils.java
@@ -0,0 +1,18 @@
+package com.liupeng.innerspring.annotation.demo2_scan;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author liupeng
+ * @Date 2018/12/26
+ */
+public class ScanUtils {
+    public static void scan() {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(CustomizeScanTest.class);
+        annotationConfigApplicationContext.refresh();
+
+        ScanClass1 injectClass = annotationConfigApplicationContext.getBean(ScanClass1.class);
+        injectClass.print();
+    }
+}
--- mvc-web/src/main/java/com/liupeng/controller/spring/beanRegister/BeanRegisterController.java
+++ mvc-web/src/main/java/com/liupeng/controller/spring/beanRegister/BeanRegisterController.java
@@ -1,5 +1,6 @@
 package com.liupeng.controller.spring.beanRegister;
 
+import com.liupeng.innerspring.annotation.demo2_scan.ScanUtils;
 import com.liupeng.spring.beanRegister.SpringBeanServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,5 +19,6 @@ public class BeanRegisterController {
     @RequestMapping("test")
     public void test() {
         System.out.println(springBeanService);
+        ScanUtils.scan();
     }
 }
